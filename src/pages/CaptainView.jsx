5001baa3c3efe28198635b30efba8b954aa6a46e
Nikhil kumar <nikhil821023@gmail.com>
2026-08-18 11:21:11 +0530

    Fix useEffect syntax and formatting in CaptainView

--- src/pages/CaptainView.jsx
+++ src/pages/CaptainView.jsx
@@ -54,7 +54,7 @@ export default function CaptainView() {
       setDashboard(JSON.parse(msg.body))
     })
   }, [tid]))
- useEffect(() => {
+  useEffect(() => {
     if (!team && tid) {
       setTeamLoading(true)
       setTeamError('')
